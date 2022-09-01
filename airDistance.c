26a200f5974b1d9e84b2711468aaadef053edad4
connorlicht <111907619+connorlicht@users.noreply.github.com>
2022-09-01 18:44:49 +0000

    added code to promt user for inputs

--- airDistance.c
+++ airDistance.c
@@ -11,10 +11,20 @@
 #include <math.h>
 
 int main(int argc, char **argv) {
-    double aLat, bLat, aAong, bLong;
+    double aLat, bLat, aLong, bLong;
 
     printf("Please input latitude of first location\n");
     scanf("%lf", &aLat);
 
+    printf("Please input longitude of first location\n");
+    scanf("%lf", &aLong);
+
+    printf("Please input latitude of second location\n");
+    scanf("%lf", &bLat);
+
+    printf("Please input longitude of second location\n");
+    scanf("%lf", &bLong);
+
+    
   return 0;
 }
