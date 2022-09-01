f24a949f254456c27b4611c392f75739025f2c9b
connorlicht <111907619+connorlicht@users.noreply.github.com>
2022-09-01 19:41:49 +0000

    it works

--- airDistance.c
+++ airDistance.c
@@ -11,36 +11,54 @@
 #include <math.h>
 
 int main(int argc, char **argv) {
+
     double aLat, bLat, aLong, bLong;
     double radianALat, radianALong, radianBLat, radianBLong;
     double sinLat, cosLat;
+    int r = 6371;
+    double cosDelta;
+
 
     printf("Please input latitude of first location\n");
     scanf("%lf", &aLat);
-
     printf("Please input longitude of first location\n");
     scanf("%lf", &aLong);
-
     printf("Please input latitude of second location\n");
     scanf("%lf", &bLat);
-
     printf("Please input longitude of second location\n");
     scanf("%lf", &bLong);
 
+
+    //Converts degrees to radian
     radianALat = (aLat / 180) * M_PI;
     //printf("%lf\n", radianALat);
-
     radianALong = (aLong / 180) * M_PI;
     //printf("%lf\n", radianALong);
-
     radianBLat = (bLat / 180) * M_PI;
     //printf("%lf\n", radianBLat);
-
     radianBLong = (bLong / 180) * M_PI;
     //printf("%lf\n", radianBLong);
 
+    //Calculation for arcos(sinLatA)(sinLatB) = x
+
     sinLat = sin(radianALat) * sin(radianBLat);
     cosLat = cos(radianALat) * cos(radianBLat);
-    
+
+    printf("%lf\n", sinLat);
+    printf("%lf\n", cosLat);
+
+    // Claculation for cos(LatA)cos(Latb)(CosDelta)(R)
+
+    cosDelta = cos(radianBLong - radianALong);
+    printf("%lf\n", cosDelta);
+
+    double distance;
+    distance = acos(sinLat + ( cosLat * cosDelta )) * r ;
+
+
+
+    printf("%lf\n", distance);
+
+
   return 0;
 }
